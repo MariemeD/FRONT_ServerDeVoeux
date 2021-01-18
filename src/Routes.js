a9b55546baddbbbdc85a635ded24015ec0745130
MarionLnd <maye43@gmail.com>
2021-01-18 09:15:52 +0100

    Ajout des dépendences et fichier des Routes

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+export default [
+    // Login route
+
+
+    // Register route
+
+
+    // Admin routes
+
+
+    // Professor routes
+]
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+import Routes from "@/Routes";
+//import { library } from '@fortawesome/fontawesome-svg-core'
+import FontAwesomeIcon from "@fortawesome/vue-fontawesome/src/components/FontAwesomeIcon";
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+})
+
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
