652e236265244d2674bda0b9dc5aec97808f231c
MarionLnd <maye43@gmail.com>
2021-02-08 11:23:43 +0100

    Continuité page détail prof

--- src/components/AdminPages/Professors/Professors/ProfessorDetails.vue
+++ src/components/AdminPages/Professors/Professors/ProfessorDetails.vue
@@ -12,14 +12,17 @@
                 </div>
                 <div class="card-body text-left">
                     <form>
-                        <div class="form-group">
-                            <label for="lastname">Nom:</label>
-                            <input id="lastname" class="form-control" type="text" :placeholder="professor.lastname" disabled>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="firstname">Prénom:</label>
-                            <input id="firstname" class="form-control" type="text" :placeholder="professor.firstname" disabled>
+                        <div class="form-row">
+                            <div class="col form-group">
+                                <label for="lastname">Nom:</label>
+                                <input id="lastname" class="form-control" type="text" :placeholder="professor.lastname" disabled>
+                            </div>
+
+                            <div class="col form-group">
+                                <label for="firstname">Prénom:</label>
+                                <input id="firstname" class="form-control" type="text" :placeholder="professor.firstname" disabled>
+                            </div>
                         </div>
 
                         <div class="form-group">
@@ -27,11 +30,14 @@
                             <input id="email" class="form-control" type="text" :placeholder="professor.email" disabled>
                         </div>
 
-                        <div class="form-check">
-                            <input id="adminInput" class="form-check-input" type="radio" :value="professor.isAdmin" disabled>
-                            <label for="adminInput">Est administrateur</label>
+                        <div class="form-group">
+                            <label for="profStatus">Statut:</label>
+                            <input id="profStatus" class="form-control" type="text" :placeholder="professor.statut" disabled>
                         </div>
 
+                        <!-- TODO Gérer l'affichage conditionnel en fonction de si le prof est responsable ou non -->
+
+                        <!-- Affichage conditionnel -->
                         <div class="form-check">
                             <input id="responsibleInput" class="form-check-input" type="radio" :value="professor.isAdmin" disabled>
                             <label for="responsibleInput">Est responsable de la filière : { nom filière }</label>
@@ -45,14 +51,21 @@
 
 <script>
     import Header from "@/components/AdminPages/Header";
+    import axios from "axios";
+
     export default {
         name: "Professor",
-        components: {Header},
+        components: { Header },
         data() {
             return {
                 idProf: this.$route.params.idProf,
                 professor: {}
             }
+        },
+        created() {
+            axios.get("").then(response => {
+                this.professor = response.data
+            })
         }
     }
 </script>
