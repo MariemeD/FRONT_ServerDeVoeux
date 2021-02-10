56f2a29ace7ce9ff46c17cdacd6c2964568e99d7
marieme <ps.marieme95@gmail.com>
2021-02-11 00:53:29 +0100

    Page de connexion

--- src/components/GeneralPages/Login.vue
+++ src/components/GeneralPages/Login.vue
@@ -1,52 +1,43 @@
 <template>
-    <!--Bleu (header) : #536895
-    Bleu (carte) : #344C80
-    Bleu "ciel" (dans les icons) : #B5B9D1
-    Gris : #D5D5D5
-    Orange : #C9893C-->
-    <div class="base">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-2"></div>
-                <div class="col-lg-6 col-md-8 login-box">
-                    <div class="col-lg-12 login-key">
-                        <i class="fa fa-key" aria-hidden="true"></i>
-                    </div>
-                    <div class="col-lg-12 login-title">
-                        Serveur de voeux
-                    </div>
-                    <div class="col-lg-12 login-title">
-                        Connexion
-                    </div>
-
-                    <div class="col-lg-12 login-form">
-                        <div class="col-lg-12 login-form">
-                            <form>
-                                <div class="form-group">
-                                    <label class="form-control-label">USERNAME</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label class="form-control-label">PASSWORD</label>
-                                    <input type="password" class="form-control" i>
-                                </div>
-
-                                <div class="col-lg-12 loginbttm">
-                                    <div class="col-lg-6 login-btm login-text">
-                                        <!-- Error Message -->
-                                    </div>
-                                    <div class="col-lg-6 login-btm login-button">
-                                        <button type="submit" class="btn btn-outline-primary">LOGIN</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-2"></div>
+  <div class="container">
+    <!-- Button to Open the Modal -->
+    <h1>Serveur de voeux </h1>
+    <h2>Département informatique - Université d'Evry</h2>
+    <h3>2020 - 2021</h3>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"> Connexion </button> <!-- The Modal -->
+    <div class="visiteur"> <a href="#" class="linkVisiteur">Liste des professeurs</a> </div>
+    <div class="visiteur"> <a href="#" class="linkVisiteur">Liste des enseignements</a> </div>
+    <div class="visiteur"> <a href="#" class="linkVisiteur">Liste des enseignements non couverts</a> </div>
+    <div class="modal fade" id="myModal">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <div class="modal-title"><p>Serveur de voeux</p></div>
+          </div> <!-- Modal body -->
+          <div class="modal-body">
+            <p class="title"> Connexion </p>
+            <form method="POST">
+              <div class="input-group"> <input class="input--style-3" type="text" placeholder="Login*" name="email"> </div>
+              <div class="input-group"> <input class="input--style-3" type="password" placeholder="Password*" name="password"> </div>
+              <div class="extra"> <a href="#"><u>I forgot my password</u></a> </div>
+              <div class="p-t-10"> <button class="btn btn--pill btn--signin" type="submit" data-target="#">CONNEXION</button> </div>
+              <!--<p class="title">or sign in using apps you love:</p>-->
+              <!--<div class="row">
+                <div class="col">
+                  <div class="p-t-10"> <button class="btn btn--pill btn--green" type="submit">SIGN IN WITH GOOGLE <img src="https://img.icons8.com/color/48/000000/google-logo.png" /> </button> </div>
                 </div>
-            </div>
+                <div class="col">
+                  <div class="p-t-10"> <button class="btn btn--pill btn--green" type="submit">SIGN IN WITH PODIO <img src="https://img.icons8.com/color/48/000000/podio.png" /> </button> </div>
+                </div>
+              </div>
+              <p class="extra new">New to teamdeck? <a href="#"><u>Sign up</u></a></p>-->
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
     export default {
@@ -62,130 +53,213 @@
 </script>
 
 <style scoped>
-    .base {
-        background: #C9893C;
-        font-family: 'Roboto', sans-serif;
-    }
+@import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
 
-    .login-box {
-        height: auto;
-        background: #344C80;
-        text-align: center;
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    }
+body {
+  background-color: rgb(58, 177, 2);
+  font-family: Poppins,serif
+}
 
-    .login-key {
-        height: 100px;
-        font-size: 80px;
-        line-height: 100px;
-        background: -webkit-linear-gradient(#27EF9F, #0DB8DE);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-    }
+@media(width:1024px) {
+  .modal-dialog {
+    max-width: 70%
+  }
+}
 
-    .login-title {
-        margin-top: 15px;
-        text-align: center;
-        font-size: 30px;
-        letter-spacing: 2px;
-        margin-top: 15px;
-        font-weight: bold;
-        color: #ECF0F5;
-    }
+.modal-content {
+  border-radius: 0.7rem
+}
 
-    .login-form {
-        margin-top: 25px;
-        text-align: left;
-    }
+.modal-header img {
+  width: 100px
+}
 
-    input[type=text] {
-        background-color: #1A2226;
-        border: none;
-        border-bottom: 2px solid #C9893C;
-        border-top: 0px;
-        border-radius: 0px;
-        font-weight: bold;
-        outline: 0;
-        margin-bottom: 20px;
-        padding-left: 0px;
-        color: #ECF0F5;
-    }
+.modal-title {
+  margin-left: auto;
+  margin-right: auto
+}
 
-    input[type=password] {
-        background-color: #1A2226;
-        border: none;
-        border-bottom: 2px solid #C9893C;
-        border-top: 0px;
-        border-radius: 0px;
-        font-weight: bold;
-        outline: 0;
-        padding-left: 0px;
-        margin-bottom: 20px;
-        color: #ECF0F5;
-    }
+.modal-header {
+  border-bottom: none;
+  padding-bottom: 0;
+  padding-top: 4vh
+}
 
-    .form-group {
-        margin-bottom: 40px;
-        outline: 0px;
-    }
+.modal-footer {
+  border-top: none
+}
 
-    .form-control:focus {
-        border-color: inherit;
-        -webkit-box-shadow: none;
-        box-shadow: none;
-        border-bottom: 2px solid #C9893C;
-        outline: 0;
-        background-color: #1A2226;
-        color: #ECF0F5;
-    }
+button:active {
+  outline: none
+}
 
-    input:focus {
-        outline: none;
-        box-shadow: 0 0 0;
-    }
+button:focus {
+  outline: none
+}
 
-    label {
-        margin-bottom: 0px;
-    }
+.modal-body {
+  text-align: center
+}
 
-    .form-control-label {
-        font-size: 10px;
-        color: #6C6C6C;
-        font-weight: bold;
-        letter-spacing: 1px;
-    }
+.title {
+  font-size: 17px;
+  color: grey
+}
 
-    .btn-outline-primary {
-        border-color: #C9893C;
-        color: #0DB8DE;
-        border-radius: 0px;
-        font-weight: bold;
-        letter-spacing: 1px;
-        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    }
+@media(min-height:768px)and(min-width:411px) {
+  .title {
+    font-size: 20px;
+    color: grey
+  }
+}
 
-    .btn-outline-primary:hover {
-        background-color: #0DB8DE;
-        right: 0px;
-    }
+form {
+  padding: 3vh
+}
 
-    .login-btm {
-        float: left;
-    }
+input {
+  outline: none;
+  margin: 0;
+  border: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  width: 100%;
+  font-size: 14px;
+  font-family: inherit
+}
 
-    .login-button {
-        padding-right: 0px;
-        text-align: right;
-        margin-bottom: 25px;
-    }
+.input-group {
+  position: relative;
+  margin-bottom: 6vh;
+  border-bottom: 1px solid rgba(204, 204, 204, 0.459)
+}
 
-    .login-text {
-        text-align: left;
-        padding-left: 0px;
-        color: #A2A4A4;
-    }
+.input--style-3 {
+  font-size: 14px;
+  color: rgb(143, 141, 141);
+  background: transparent
+}
 
-    .loginbttm {
-        padding: 0px;
-    }
+.input--style-3::-webkit-input-placeholder {
+  color: rgb(143, 141, 141)
+}
+
+.input--style-3:-moz-placeholder {
+  color: #ccc;
+  opacity: 1
+}
+
+.input--style-3::-moz-placeholder {
+  color: #ccc;
+  opacity: 1
+}
+
+.input--style-3:-ms-input-placeholder {
+  color: #ccc
+}
+
+.btn {
+  display: inline-block;
+  line-height: 42px;
+  padding: 0 33px;
+  font-family: Poppins,serif;
+  cursor: pointer;
+  color: #fff;
+  -webkit-transition: all 0.4s ease;
+  -o-transition: all 0.4s ease;
+  -moz-transition: all 0.4s ease;
+  transition: all 0.4s ease;
+  font-size: 18px;
+  width: 100%;
+}
+
+.btn--pill {
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  border-radius: 30px;
+  border: 2px solid
+}
+
+.btn--green {
+  background: transparent;
+  border-color: #65d849;
+  color: #65d849;
+  font-size: 12px;
+  padding: 0
+}
+
+@media(max-width:768px) {
+  .btn--green {
+    font-size: 8px
+  }
+}
+
+.btn--green img {
+  height: 15px;
+  width: 15px
+}
+
+.btn--signin {
+  background: #ccc;
+  color: rgb(109, 107, 107);
+  font-size: 13px;
+  border-color: #ccc;
+  margin-bottom: 3vh
+}
+
+.extra {
+  padding-bottom: 4vh;
+  color: rgb(143, 141, 141);
+  font-size: 13px
+}
+
+.extra a {
+  color: rgb(143, 141, 141);
+  font-size: 13px
+}
+
+.col {
+  padding: 2vh 10px 4vh
+}
+
+.new {
+  padding-bottom: 0
+}
+
+.btn-primary {
+  width: 40%;
+  margin: 10% 30% 5% 30%;
+  background-color: #C9893C;
+}
+
+.btn:focus {
+  box-shadow: none;
+  outline: none
+}
+
+.container{
+  background-color: #344C80;
+  padding-top: 10%;
+  padding-bottom: 10%;
+  border-radius: 20px;
+  box-shadow: 3px 3px 10px 5px #000;
+}
+
+h1,h2,h3{
+  color: white;
+  /*border: 2px solid red;*/
+}
+
+
+a:link, a:visited, a:hover, a:active{
+  text-decoration: none;
+  color: white;
+}
+
+.visiteur{
+  border: 2px solid transparent;
+  padding-bottom: 1.5%;
+}
+
 </style>
