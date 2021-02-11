8b5f01d5786bfb4e214cdb091f3abe783a52eef1
MarionLnd <maye43@gmail.com>
2021-02-11 18:15:16 +0100

    [ADMIN] Pages Statuts (GET, POST, PUT) OK + legère modif Homepage

--- src/components/AdminPages/Professors/Status/AddStatus.vue
+++ src/components/AdminPages/Professors/Status/AddStatus.vue
@@ -2,18 +2,141 @@
     <div>
         <Header />
         <h1 class="pt-5">Ajouter un statut</h1>
+
+        <!-- ALERTS -->
+        <div class="container mb-2" v-show="formData.errorOnSubmit || formData.submitted">
+            <transition :duration="5000" name="slide-fade">
+                <div class="alert"
+                     :class="{'alert-success': formData.submitted, 'alert-danger': formData.errorOnSubmit}">
+                    <p class="text-center">
+                        {{ alertMessage }}
+                        <router-link :to="{name: 'status'}" v-if="formData.submitted" class="mb-auto">
+                            <span id="returnToStatus" class="statusActionBtn mb-3">Retour à la page des status</span>
+                        </router-link>
+                    </p>
+                </div>
+            </transition>
+        </div>
+
+
+        <div class="container">
+            <form @submit.prevent="sendForm">
+                <div class="form-group">
+                    <label for="inputStatusName" class="form-label">Nom du statut :</label>
+                    <input type="text"
+                           class="form-control"
+                           id="inputStatusName"
+                           placeholder="Entrez l'intitulé du statut"
+                           v-model.lazy="formData.statusName"
+                           required>
+                </div>
+                <div class="form-group">
+                    <label for="inputMandatoryHours" class="form-label">Nombre d'heures statuaires :</label>
+                    <input type="number"
+                           step="0.01"
+                           min="0"
+                           class="form-control"
+                           id="inputMandatoryHours"
+                           placeholder="Exemple: 100"
+                           v-model.lazy="formData.mandatoryHours"
+                           required>
+                    <small id="mandatoryHoursHelp" class="form-text text-muted text-left">
+                        Le nombre d'heures qu'un professeur doit <strong>obligatoirement</strong> effectuer
+                    </small>
+                </div>
+                <div class="form-group">
+                    <label for="inputExtraHours" class="form-label">Nombre d'heures supplémentaires :</label>
+                    <input type="number"
+                           step="0.01"
+                           min="0"
+                           class="form-control"
+                           id="inputExtraHours"
+                           placeholder="Exemple: 90"
+                           v-model.lazy="formData.extraHours"
+                           required>
+                    <small id="extraHoursHelp" class="form-text text-muted text-left">
+                        Le nombre d'heures supplémentaires qu'un professeur peut effectuer
+                    </small>
+                </div>
+                <button type="submit" class="btn statusActionBtn">Ajouter le nouveau statut</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
 import Header from "@/components/AdminPages/Header";
+import axios from 'axios'
 
 export default {
     name: "AddStatus",
-    components: {Header}
+    components: { Header },
+    data() {
+        return {
+            alertMessage: '',
+            formData: {
+                statusName: null,
+                mandatoryHours: null,
+                extraHours: null,
+                submitted: false,
+                errorOnSubmit: false
+            }
+        }
+    },
+    methods: {
+        resetData(resetError) {
+            if (resetError) {
+                this.formData.errorOnSubmit = false
+            }
+            this.formData.statusName = null
+            this.formData.mandatoryHours = null
+            this.formData.extraHours = null
+            this.formData.submitted = false
+        },
+        sendForm() {
+            if (this.formData.statusName && this.formData.mandatoryHours && this.formData.extraHours) {
+                const newStatusAdded = {
+                    name: this.formData.statusName,
+                    mandatoryHours: this.formData.mandatoryHours,
+                    extraHours: this.formData.extraHours
+                }
+
+                axios.post('https://back-serverdevoeux.herokuapp.com/api/status', newStatusAdded).then((response) => {
+                    console.log(response)
+                    this.alertMessage = "Le nouveau statut a été ajouté avec succès !"
+                    this.formData.submitted = true
+                }).catch(error => {
+                    this.alertMessage = "Le nouveau statut n'a pas pu être ajouté. Veuillez réessayer"
+                    this.formData.errorOnSubmit = true
+                    console.error(error)
+                })
+                //this.resetData(true)
+                this.formData.submitted = false
+            } else {
+                this.formData.errorOnSubmit = true
+                this.alertMessage = "Veuillez remplir tous les champs du formulaire."
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.statusActionBtn {
+    color: #536895;
+    border-color: #536895;
+}
+.statusActionBtn:hover {
+    color: #2c3e50;
+}
+.form-label {
+    margin-bottom: 0.40em;
+    float: left !important;
+}
+#returnToStatus {
+    text-decoration: underline;
+}
+.alert p {
+    margin-bottom: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import VueRouter from "vue-router";
 import Routes from "@/Routes";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import FontAwesomeIcon from "@fortawesome/vue-fontawesome/src/components/FontAwesomeIcon";
-import { faServer, faUsers, faBook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faServer,
+  faUsers,
+  faBook,
+  faSortDown,
+  faSortUp,
+  faSort,
+  faTrash,
+  faEdit
+} from "@fortawesome/free-solid-svg-icons";
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -16,6 +25,11 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 library.add(faServer)
 library.add(faUsers)
 library.add(faBook)
+library.add(faSortUp)
+library.add(faSortDown)
+library.add(faSort)
+library.add(faTrash)
+library.add(faEdit)
 
 const router = new VueRouter({
   routes: Routes,
